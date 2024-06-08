dd9139f6aee276ff87b8609daf1dc1c6abad666f
prashantcode79 <9846064078Z@gmail.com>
2024-06-08 16:07:46 +0545

    concept of private and presence channel and made controller for group chat

--- app/Events/GroupChatMessage.php
+++ app/Events/GroupChatMessage.php
@@ -11,7 +11,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class MessageSent implements ShouldBroadcastNow
+class GroupChatMessage implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
@@ -33,7 +33,7 @@ class MessageSent implements ShouldBroadcastNow
     public function broadcastOn(): array
     {
         return [
-            new PrivateChannel('messenger'),
+            new PresenceChannel('group_chat.'.$this->message->group_id)
         ];
     }
 }
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -21,7 +21,26 @@ createInertiaApp({
 });
 
 
-Echo.channel(`messenger`)
+Echo.private(`messenger`)
     .listen('MessageSent', (e) => {
         console.log(e.message);
+    });
+
+
+var roomId = 1;
+Echo.join(`group_chat.${roomId}`)
+    .here((users) => {
+        console.log(users)
+    })
+    .joining((user) => {
+        console.log(user.name);
+    })
+    .leaving((user) => {
+        console.log(user.name);
+    })
+    .listen('GroupChatMessage', (e) => {
+        console.log(e);
+    })
+    .error((error) => {
+        console.error(error);
     });
