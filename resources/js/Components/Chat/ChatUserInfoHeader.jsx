522644ce21e1ef32823b96a0ce5574281e17ef41
prashantcode79 <9846064078Z@gmail.com>
2024-06-25 21:39:40 +0545

    managed UI smooth and all messages visible

--- resources/js/Components/Chat/ChatUserInfoHeader.jsx
+++ resources/js/Components/Chat/ChatUserInfoHeader.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function ChatUserInfoHeader({receiver}) {
     return (
         <>
-            <div className="msg-view-header sticky top-0 justify-between items-center flex py-2 border-b border-slate-100">
+            <div className="msg-view-header sticky top-0 justify-between items-center flex py-2 border-b border-slate-100 bg-white">
                 <div className="profiling flex align-middle justify-center">
                     <div className="pp">
                         <img className="h-9 pe-3" src="https://www.svgrepo.com/show/384674/account-avatar-profile-user-11.svg" alt="" />
--- resources/js/Pages/Chat/Chat.jsx
+++ resources/js/Pages/Chat/Chat.jsx
@@ -11,16 +11,16 @@ export default function Chat(props) {
             user={props.auth.user}
         >
             <div>
-                <div className="main-container overflow-hidden bg-white flex h-screen">
-                    <div className="left basis-2/6 pt-3 overflow-x-hidden overflow-y-auto flex-col border-e-2">
+                <div className="main-container overflow-hidden bg-white flex h-[520px]">
+                    <div className="left basis-2/6 pt-3 overflow-auto flex-col border-e-2 no-scrollbar">
                         <ChatSidebar recentMessages={props.recentMessages} />
                     </div>
-                    <div className="right overflow-hidden basis-4/6 flex-col relative px-4">
+                    <div className="right overflow-hidden basis-4/6 flex-col relative px-4 h-100">
                         {
                             props.receiver?.id ?
                                 <>
                                     <ChatUserInfoHeader receiver={props.receiver}/>
-                                    <div className="msg-content overflow-y-auto overflow-x-hidden top-33 flex-col">
+                                    <div className="msg-content overflow-y-auto overflow-x-hidden top-33 flex-col h-[83%] no-scrollbar">
                                         <ChatMessages msg={props.messages} auth_id={props.auth?.user?.id}/>
                                     </div>
                                     <div className="w-full input-field flex fixed bottom-0 mt-2 mb-1">
